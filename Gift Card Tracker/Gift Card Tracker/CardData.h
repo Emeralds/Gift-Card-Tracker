9674e0faf40b91efe5d242075683601cbb64f6ee
Benjamin King <benjaminking@new-host-3.home>
2013-06-02 21:14:43 -0400

    added base for data model

---
+++ Gift Card Tracker/Gift Card Tracker/CardData.h
@@ -0,0 +1,13 @@
+//
+//  CardData.h
+//  Gift Card Tracker
+//
+//  Created by Benjamin King on 6/2/13.
+//  Copyright (c) 2013 Benjamin King. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardData : NSObject
+
+@end
---
+++ Gift Card Tracker/Gift Card Tracker/CardData.m
@@ -0,0 +1,13 @@
+//
+//  CardData.m
+//  Gift Card Tracker
+//
+//  Created by Benjamin King on 6/2/13.
+//  Copyright (c) 2013 Benjamin King. All rights reserved.
+//
+
+#import "CardData.h"
+
+@implementation CardData
+
+@end
